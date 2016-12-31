de7e78aeaa7d1365ba64469aad55690eb6636020
Harinder Singh <harindergodhra@gmail.com>
2016-12-31 16:12:34 +0530

    Design modified

--- src/controllers/appController.js
+++ src/controllers/appController.js
@@ -1,33 +1,43 @@
 app.controller("CaseChangerController",['$scope',function($scope){
 
-    //$scope.textBox=$scope.textBox.toString().toUpperCase();
+    $scope.intialize=function()
+    {
+        $scope.disableButtons=true;
+        $scope.disableButtons=false;
+        $scope.textBox="Type or paste your text here and click on desired button to see magic."
+    }
 
+   /*For all letters in Upper Case*/
    $scope.allUpperCase = function()
    {
+       $scope.disableButtons=true;
        var input = $scope.textBox.toString();
-
-        /*For all letters in Upper Case*/
         $scope.textBox=input.toUpperCase();
+        $scope.disableButtons=false;
    }
+
+    /*For all letters in Upper Case*/
    $scope.allLowerCase = function()
    {
+       $scope.disableButtons=true;
        var input = $scope.textBox.toString();
-
-        /*For all letters in Upper Case*/
         $scope.textBox=input.toLowerCase();
+        $scope.disableButtons=false;
    }
-    
+   
+    /*For only first letters in Upper Case*/    
    $scope.firstLetterUpperCase = function()
    {
-       var input = $scope.textBox.toString();
-       /*For only first letters in Upper Case*/       
+       $scope.disableButtons=true;
+       var input = $scope.textBox.toString();      
         $scope.textBox= input.replace(/\w+\S*/g, function(word){return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();});
-        
+        $scope.disableButtons=false;
    }
 
     /*For reverse Case*/         
     $scope.reverseCase= function()
     {
+        $scope.disableButtons=true;
         var input = $scope.textBox.toString();
         $scope.textBox=function(){
                 var c='';
@@ -39,12 +49,14 @@ app.controller("CaseChangerController",['$scope',function($scope){
                 }
                 return res;
             }();
+            $scope.disableButtons=false;
     }
 
 
     /*For StUdLyCaPs Case*/         
     $scope.studlyCaps= function()
     {
+        $scope.disableButtons=true;
         var input = $scope.textBox.toString();
         $scope.textBox = function(){
                 var c='';
@@ -56,13 +68,16 @@ app.controller("CaseChangerController",['$scope',function($scope){
                 }
                 return res;
             }();
+            $scope.disableButtons=false;
     }
     
     /*For Sentence Case*/
     $scope.sentenceCase= function()
     {
+        $scope.disableButtons=true;
         var input = $scope.textBox.toString();
         $scope.textBox=input.replace(/[^\.|\!|\?|\n|\r]+[\.|\!|\?|\n|\r]\s|[^\.|\!|\?|\n|\r]*/g, function(word){return word.charAt(0).toUpperCase() + word.substr(1).toLowerCase();});
+        $scope.disableButtons=false;
     } 
 
 }]);
